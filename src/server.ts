38d125e360291cd6d21b73386481dc14b06aec3d
unknown00p <rajpoint8976@gmail.com>
2026-01-01 19:41:21 +0530

    Refactor server setup: add cookie-parser and cors middleware; update README and package.json

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,23 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-dotenv.config()
+dotenv.config();
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET, POST, PUT, DELETE, OPTIONS, PATCH",
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 
-app.listen(()=>{
-    console.log("server started")
-})
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("server listining on ", port);
+});
