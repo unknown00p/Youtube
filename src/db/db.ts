5490cd353d09dadcdab93e057aa3204e2571c104
unknown00p <rajpoint8976@gmail.com>
2026-01-04 19:30:16 +0530

    Improve database connection handling: enhance comments for clarity, add server selection timeout, and specify socket timeout for better performance.

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,7 +3,7 @@ import { config } from "../config/config.ts";
 
 export async function connectDB() {
     try {
-        //
+        // note: the socket connection only be created when a Database query request arrives othervise the pool will be empty untill we define the default socket connection in minPoolSize
         await mongoose.connect(config.DATABASE_URL,{
             // Maximum number of concurrent connections the pool can have.
             // If more queries arrive than available connections, the pool will create new connections up to this limit.
@@ -12,7 +12,12 @@ export async function connectDB() {
             // Minimum number of connections to keep open and ready.
             // Ensures that the first queries do not experience a cold-start latency.
             minPoolSize: 2,
-            
+
+            // Wait 5 seconds before failing if the Database is unreachable
+            serverSelectionTimeoutMS: 5000, 
+
+            // Close sockets after 45 seconds of inactivity
+            socketTimeoutMS: 45000,
         })
         console.log("Connected to MongoDB successfully");
     } catch (error) {
