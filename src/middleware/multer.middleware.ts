1dc1068d742dee154ab94a0d0285a8c2d907fbb1
unknown00p <rajpoint8976@gmail.com>
2026-01-15 21:49:13 +0530

    created multer middleware for file upload

---
+++ src/middleware/multer.middleware.ts
@@ -0,0 +1,24 @@
+import multer from "multer";
+import fs from "fs";
+import path from "path";
+import crypto from "crypto";
+
+const uploadDir = path.join(process.cwd(), "uploads");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadDir);
+  },
+
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const uniqueName = crypto.randomUUID();
+    cb(null, `${uniqueName}${ext}`);
+  },
+});
+
+export const upload = multer({ storage: storage });
